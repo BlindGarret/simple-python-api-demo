139fc3282eeab439a83ec153ec9631c1b41c43fb
Lucas Roe <lroe2930@gmail.com>
2023-09-16 00:38:44 -0400

    adding schema and table name

--- app/db/linkme.py
+++ app/db/linkme.py
@@ -1,6 +1,8 @@
-from sqlmodel import SQLModel, Field
+from sqlmodel import SQLModel, Field, MetaData
 
 
 class Link(SQLModel, table=True):
+    __tablename__ = "links"
+    metadata = MetaData(schema="data")
     path: str = Field(primary_key=True)
     short_path: str
